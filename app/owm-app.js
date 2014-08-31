c85a74e584f86776371324ca1f2980c7cf68cb4b
Israel Bautista <bautista.israel@rocketmail.com>
2014-08-31 16:31:35 -0500

    routes added

---
+++ app/owm-app.js
@@ -0,0 +1,72 @@
+angular.module('OWMApp', ['ngRoute'])
+	.value('owmCities',
+		['New York', 'Dallas', 'Chicago', 'Boston'])
+
+
+	.config(function($routeProvider){
+		$routeProvider
+		.when('/', {
+			templateUrl: './home.html',
+			controller: 'HomeCtrl'
+		})
+		.when('/cities/:city', {
+			templateUrl: './city.html',
+			controller: 'CityCtrl',
+			resolve: {
+				city: function(owmCities, $route, $location){
+					var city = $route.current.params.city;
+					if(owmCities.indexOf(city) == -1 ){
+						$location.path('/error');
+						return;
+					}
+					return city;
+				}
+			}
+		})
+		.when('/error', {
+			template: '<p>Error Page Not Found</p>'
+		})
+		.otherwise({
+			redirectTo: '/error'
+		});
+	})
+
+
+	// intercepting the routing event with root scope
+	.run(function($rootScope, $location, $timeout){
+		$rootScope.$on('$routeChangeError', function(){
+			$location.path('/error');
+		});
+		$rootScope.$on('$routeChangeStart', function(){
+			$rootScope.isLoading = true;
+		});
+		$rootScope.$on('$routeChangeSuccess', function(){
+			$timeout(function(){
+				$rootScope.isLoading = false;
+			}, 1000);
+		});
+	})
+
+
+	.controller('HomeCtrl', function($scope){
+		
+	})
+
+
+	.controller('CityCtrl', function($scope, city){
+		$scope.city = city;
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
